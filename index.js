2eeb7c3e0ab8d892af58427c0d70cad2de17d2f7
Ryan <grossry@oregonstate.edu>
2018-12-31 16:33:42 -0500

    Added swagger.json
    Created base documentation

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
 app.get('/api/v1/ping', function(req, res) {
     res.sendStatus(200)
 })
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(3000)
